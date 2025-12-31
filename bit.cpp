42fc1d6e56f7d36ea688495e57d64b5860cbf23f
pratim chakraborty <aryanpratim86@gmail.com>
2025-12-31 14:49:58 +0530

    Add files via upload

--- bit.cpp
+++ bit.cpp
@@ -1,35 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<long long> get_divisors(long long num) {
-    vector<long long> divisors;
-    for (long long i = 1; i * i <= num; ++i) {
-        if (num % i == 0) {
-            divisors.push_back(i);
-            if (i != num / i) divisors.push_back(num / i);
-        }
-    }
-    sort(divisors.begin(), divisors.end());
-    return divisors;
-}
 
-int main() {
-    long long w, h, d, n;
-    cin >> w >> h >> d >> n;
-    auto divisors = get_divisors(n);
-    for (auto x : divisors) {
-        if (w % x != 0) continue;
-        for (auto y : divisors) {
-            if (h % y != 0) continue;
-            long long prod = x * y;
-            if (prod > n || n % prod != 0) continue;
-            long long z = n / prod;
-            if (d % z == 0) {
-                cout << (x - 1) << " " << (y - 1) << " " << (z - 1) << endl;
-                return 0;
-            }
-        }
+
+int main(){
+    int array[250];
+    int n, p, q;
+    cin >> n >> p;
+    for(int i = 0;i<p;i++){
+        cin >> array[i];
     }
-    cout << -1 << endl;
+    cin >> q;
+    for(int i = p;i<p+q;i++){
+        cin >> array[i];
+    }
+    sort(array, array+(p+q));
+    int count =0;
+    for(int i = 0;i < p+q;i++){
+        if(array[i] != array[i+1]){
+            count++;
+        }
+
+    }
+    if(count == n){
+        cout << "I become the guy." << endl;
+    }else{
+        cout << "Oh, my keyboard!" << endl;
+    }
+
     return 0;
 }
